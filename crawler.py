bc9eae458fcec51b374a65bce0fc58214bdd8c44
Faiz Khan <faizkhan00@gmail.com>
2015-03-27 13:30:33 -0500

    log format

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@
 
 # Written by Jonathon Vogel, 2014
 
-import bs4
+import bs4, time
 import feedparser
 import requests
 import slack
@@ -23,6 +23,8 @@ SLACK_USERNAME = 'Bitcoin-Talk-Bot'
 SLACK_CHANNEL = '#general'
 
 BITCOIN_TALK_RSS = 'https://bitcointalk.org/index.php?type=rss;action=.xml&limit=100'
+
+def addtime(): return time.ctime() + ':'
 
 def string_find_all(s, needle):
     """A generator that finds all occurences of needle in s."""
@@ -62,9 +64,10 @@ def check_post_strings(url, kwd=KEYWORDS):
     post_id_elem = html.find('a', href=url)
     if post_id_elem is None: # bitcoin talk returning bad HTML
         with open("btb_log.txt", "a") as btblog:
-            btblog.write('\nBad HTML (503?), bailing...')
+            btblog.write(addtime() + '<br>')
+            btblog.write('Bad HTML (503?), bailing...')
             btblog.write(html[:100])
-            btblog.write('\n')
+            btblog.write('<br>')
         raise Exception('Bad HTML, possible 503')
     post = post_id_elem.find_next('div', {'class': 'post'})
 
@@ -98,13 +101,14 @@ def check_btc_talk(last_post_checked):
         if 'id' not in entry or (last_post_checked is not None and get_post_id(entry['id']) <= get_post_id(last_post_checked)):
             continue
         with open("btb_log.txt", "a") as btblog:
-            btblog.write('\n')
+            btblog.write(addtime() + '<br>')
             btblog.write(entry['id'])
+
         try:
             mentions = check_post_strings(entry['id'], KEYWORDS)
             if len(mentions):
                 with open("btb_log.txt", "a") as btblog:
-                    btblog.write('\n')
+                    btblog.write(addtime() + '<br>')
                     btblog.write('Found a mention, posting to slack...')
                 slack.chat.post_message(SLACK_CHANNEL, MESSAGE_FORMAT.format(entry['title'], entry['id'], '\n'.join(mentions)), username=SLACK_USERNAME)
             last_post_checked = entry['id']
@@ -112,7 +116,7 @@ def check_btc_talk(last_post_checked):
             if isinstance(e, KeyboardInterrupt):
                 raise e
             with open("btb_log.txt", "a") as btblog:
-                btblog.write('\n')
+                btblog.write(addtime() + '<br>')
                 btblog.write('Unhandled exception, retrying feed parse at exception point')
             traceback.print_exc()
             break
@@ -123,7 +127,7 @@ def main():
     """Loop and exception handling"""
     last_post_checked = feedparser.parse(BITCOIN_TALK_RSS)['entries'][0]['id'] # don't spend a bunch of time parsing old comments
     with open("btb_log.txt", "a") as btblog:
-        btblog.write('\n')
+        btblog.write(addtime() + '<br>')
         btblog.write('Running')
     while True:
         try:
@@ -132,7 +136,7 @@ def main():
         except Exception as e:
             if isinstance(e, KeyboardInterrupt):
                 with open("btb_log.txt", "a") as btblog:
-                    btblog.write('\n')
+                    btblog.write(addtime() + '<br>')
                     btblog.write('Being killed! Exiting...')
                 break
             print('Unexpected exception, trying to continue...')
